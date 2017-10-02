5ba3544f74a88d8c0fabed05d2967682c3e42471
emanoelmlsilva <emanoelmlsilva.@gmail.com>
2017-10-01 21:05:35 -0300

    listagem

---
+++ Livropython/list.lista6.46.py
@@ -0,0 +1,9 @@
+tabela = {"Alface":0.45,"Batata":1.20,"Tomate":2.30,"Feijão":1.50}
+print(tabela["Tomate"])
+print(tabela)
+tabela["Tomate"] = 2.50
+print(tabela["Tomate"])
+tabela["Cebola"] = 1.20
+print(tabela)
+print("manga" in tabela) #verificando se uma chaves existe no dicionario operador in
+print("Tomate" in tabela)
---
+++ Livropython/list.lista6.49.py
@@ -0,0 +1,4 @@
+tabela = {"Alface":0.45,"Batata":1.20,"Tomate":2.30,"Feijão":1.50}
+print(tabela.keys()) #obter todas as chaves do dicionario
+print(tabela.values()) #obter todos os valores do dicionario
+print(type(tabela))
